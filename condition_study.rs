12dd320ce6923aa53407b1ad813d450d12ffd8f1
l050101 <l01062506156@gmail.com>
2022-05-25 23:26:58 +0900

    still study...haha...

--- condition_study.rs
+++ condition_study.rs
@@ -1,10 +1,16 @@
 fn array(){
-	let arr = ["hello","goodbye"];
-	println!("{}",arr[0]);
-	let ann = [1;5];
-	println!("{} {}",ann[0],ann[1]);
-}
+    let mut array = [0;100];
+    
+    array[0]=10;
+    array[1]=20;
+    array[3]=100;
+    array[4]=19;
+    array[5]=10;
+    array[7]=10;
+    array[6]=10;
+    println!("{:?}",array);
 
+}
 fn vector(){
 	let mut fruits = Vec::new();
 	fruits.push("apple");
@@ -13,10 +19,11 @@ fn vector(){
 	println!("{:?}",fruits);
 	// fruits.push(1); 안됨
 	println!("{:?}",fruits.pop());
+
+    println!("{:?}",fruits[1]);
 }
 
 fn main(){
 	array();
 	vector();
 }
-
---
+++ vimTest.rs
@@ -0,0 +1,5 @@
+fn main(){
+    let a:i32=10;
+    let b:i32=11;
+    print!("{}",a+b);
+}
