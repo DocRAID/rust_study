5037554139163cb16f4802f01999ae021235f115
l050101 <l01062506156@gmail.com>
2022-06-14 09:35:47 +0900

    rust app make....ing

---
+++ greprs/readme.md
@@ -0,0 +1,2 @@
+let args: Vec<String> = env::args().collect();
+쿼리 받기 
--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -5,16 +5,25 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("Something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+struct Config{
+    query:String,
+    filename:String,
+}
+fn parse_config(args: &[String]) -> Config{
+    //어디서 오류가 나는지, 수정을 쉽게 하기위해 모듈로 나눈다.
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config{query,filename}
 }
